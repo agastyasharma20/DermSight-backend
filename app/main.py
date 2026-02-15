ef3ce530df1deaa0599448fd6475d8ceff4f7b6f
agastyapy <agastyaanoopsharma@gmail.com>
2026-02-15 06:40:04 +0530

    Fixed CORS for production

--- app/main.py
+++ app/main.py
@@ -8,9 +8,17 @@ app = FastAPI(
     version="1.0.0"
 )
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://derm-sight-frontend.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Hackathon only
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
